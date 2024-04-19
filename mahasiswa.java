e4990a41d5d46ac885743fd21fa8842bbcfe75b6
angelevy <145308211+angelevy@users.noreply.github.com>
2024-04-19 22:53:05 +0700

    Add files via upload

---
+++ mahasiswa.java
@@ -0,0 +1,51 @@
+public class mahasiswa {
+    private String nim;
+    private String kelas;
+    private String nama;
+    private double nilai;
+
+    public mahasiswa(String nim, String kelas, String nama, double nilai) {
+        this.nim = nim;
+        this.kelas = kelas;
+        this.nama = nama;
+        this.nilai = nilai;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getNilai() {
+        return nilai;
+    }
+
+    public void setNilai(double nilai) {
+        this.nilai = nilai;
+    }
+
+    @Override
+    public String toString() {
+        return "NIM : " + nim + " Nama : " + nama + " Kelas : " + kelas + " ";
+
+    }
+}
